515f5bada487a81dd01ac4f91803441b4c69c9cb
clsong <chuliangsong1994@gmail.com>
2024-11-08 14:24:25 -0800

    init

---
+++ .vdoc.r
@@ -0,0 +1,82 @@
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| eval: false
+eat(dressed(shower(brush(wakeup(you)))))
+```
+#
+#
+#
+#| eval: false
+you_w <- wakeup(you)
+you_bw <- brush(you_w)
+you_sbw <- shower(you_bw)
+...
+```
+#
+#
+#| eval: false
+you %>% 
+  wakeup() %>% 
+  brush() %>% 
+  shower() %>% 
+  get_dressed() %>% 
+  eat() 
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
