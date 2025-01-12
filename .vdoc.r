6163b91540eb1686d5999c497b310c7d13f53823
Jane Doe <jane@example.org>
2025-01-12 00:17:32 -0800

    remove warning

--- .vdoc.r
+++ .vdoc.r
@@ -13,82 +13,35 @@
 #
 #
 #
-numbers <- c(1, 2, 3, 4, 5)  # <1>
-numbers # <2>
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+print("Hello, World!") # <1>
 ```
 #
 #
 #
 #
+3 + 2
 #
 #
 #
 #
-#
-#
-#
-#
-#
-#
-#
-#
-#
-numbers <- c(1.1, 2.2, 3.3, 4.4, 5.5) # <1>
-characters <- c("a", "b", "c") # <2>
-logicals <- c(TRUE, FALSE, TRUE) # <3>
-factors <- factor(c("a", "b", "c")) # <4>
-```
-#
-#
-#
-#
-#
-#
-#
-#
-#
-class(numbers) 
-```   
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-library(tidyverse) # <1>
-tibble( # <2>
-    x = c(1, 2, 3),  # <3>
-    y = c("a", "b", "c"), # <4>
-    z = c(TRUE, FALSE, TRUE) # <5>
-) # <6>
-```
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-tribble( # <1>
-  ~x, ~y, # <2>
-  1, "a", # <3>
-  2, "b", 
-  3, "c"
-)
+x <- 3 + 2 # <1>
+x       # <2>
 ```
 #
 #
@@ -112,9 +65,35 @@ tribble( # <1>
 #
 #
 #
-# install.packages("palmerpenguins")   # <1>
-library(palmerpenguins) # <2>
-data(package = "palmerpenguins") # <3>
+#
+#
+#
+#
+#
+#| eval: false
+install.packages("ggplot2")
+#
+#
+#
+#
+#| warning: false
+library(ggplot2)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| eval: false
+# install.packages("devtools") # <1>
+library(devtools) # <2>
+devtools::install_github("cttobin/ggthemr") # <3>
 ```
 #
 #
@@ -123,20 +102,57 @@ data(package = "palmerpenguins") # <3>
 #
 #
 #
-penguins # <1>
-``` 
 #
 #
-#
-#
-#
-#
-#
-#
-#
-pacman::p_load(skimr) # <1>
-skim(penguins) # <2>
+# install.packages("pacman") # <1>
+library(pacman) # <2>
+p_load(ggplot2) # <3>
 ```
+#
+#
+#
+#
+#
+#
+#
+#
+pacman::p_load(ggplot2) # <1>
+```
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| eval: false
+install.packages("tidyverse")
+#
+#
+#
+#
+#| warning: false
+library(tidyverse)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+suppressPackageStartupMessages(library(tidyverse))
 #
 #
 #
