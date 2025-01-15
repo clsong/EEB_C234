c362d16af87e71eb3dcbb2754bb461ea06e5473c
Jane Doe <jane@example.org>
2025-01-15 00:43:53 -0800

    update histroy

--- .vdoc.r
+++ .vdoc.r
@@ -16,10 +16,19 @@
 #
 #
 #
+pacman::p_load(palmerpenguins) 
 #
-#| eval: false
-x <- 1+1 # <1>
-x^2 # <2>
+#
+#
+#
+#| warning: false
+library(ggplot2) # <1>
+ggplot( # <2>
+  data = penguins, # <3>
+  aes(x = bill_length_mm, # <4> 
+      y = bill_depth_mm) # <5>
+) +  # <6>
+  geom_point() # <7>
 ```
 #
 #
@@ -28,12 +37,135 @@ x^2 # <2>
 #
 #
 #
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
 #| eval: false
-#| source-line-numbers: "4"
-x <- 1+1 
-x^2 
-x^3 # <1>
+ggplot(
+  data = <DATA>, # <1>
+  aes(x = <X>, y = <Y>) # <2>
+) + # <3>
+  <GEOM_FUNCTION>(aes(...)) # <4>
 ```
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| warning: false
+#| source-line-numbers: "8"
+ggplot(
+  data = penguins,
+  aes(
+    x = bill_length_mm,
+    y = bill_depth_mm
+  )) +
+  geom_point(aes(color = species)) # <1>
+```
+#
+#
+#
+#
+#| warning: false
+#| source-line-numbers: "8"
+ggplot(
+  data = penguins,
+  aes(
+    x = bill_length_mm,
+    y = bill_depth_mm 
+  )) +
+  geom_point(aes(color = species, shape = species)) # <1>
+```
+#
+#
+#
+#
+#
+#| warning: false
+#| source-line-numbers: "9"
+ggplot(
+  data = penguins,
+  aes(
+    x = bill_length_mm,
+    y = bill_depth_mm 
+  )) +
+  geom_point(aes(color = species, shape = species)) +
+  theme_bw() # <1>
+```
+#
+#
+#
+#
+#
+#
+#
+#| warning: false
+# install.packages("jtools") # <1>
+ggplot(
+  data = penguins,
+  aes(
+    x = bill_length_mm,
+    y = bill_depth_mm,
+    color = species,
+    shape = species
+  )) +
+  geom_point() +
+  jtools::theme_nice() # <2>
+```
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
 #
 #
 #
