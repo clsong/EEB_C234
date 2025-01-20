85993c07faab419964be879f56daf48eb4045010
Jane Doe <jane@example.org>
2025-01-19 16:21:36 -0800

    updoate class

--- .vdoc.r
+++ .vdoc.r
@@ -4,18 +4,6 @@
 #
 #
 #
-#| warning: false
-library(ggplot2)
-library(palmerpenguins)
-ggplot(
-    data = penguins,
-    aes(
-        x = bill_length_mm,
-        y = bill_depth_mm
-    )
-) +
-  geom_point(aes(color = species)) +
-  theme_minimal()
 #
 #
 #
@@ -23,48 +11,25 @@ ggplot(
 #
 #
 #
+#
+#
+#
+#
+#
+data_nobel <- read.csv("http://clsong.com/assets/class_data/data_nobel.csv")
 #
 #
 #
 #
 #| warning: false
-library(ggplot2)
-library(palmerpenguins)
-ggplot(
-    data = penguins,
-    aes(
-        x = bill_length_mm,
-        y = bill_depth_mm
-    )
-) +
-  geom_point(aes(color = species)) +
-  jtools::theme_nice() # <1>
-```
-#
-#
-#
-#
-#
-#| eval: false
-pacman::p_load(devtools)
-install_github('Mikata-Project/ggthemr')
-#
-#
-#
-#
-#| warning: false
-library(ggplot2)
-library(palmerpenguins)
+library(tidyverse)
 
 ggplot(
-    data = penguins,
-    aes(
-        x = bill_length_mm,
-        y = bill_depth_mm
-    )
+    data = data_nobel,
+    aes(x = Chocolate, y = Nobel)
 ) +
-  geom_point(aes(color = species)) +
-  ggthemr::ggthemr('fresh', set_theme = FALSE)$theme
+    geom_point() +
+    theme_bw()
 #
 #
 #
@@ -74,35 +39,12 @@ ggplot(
 #
 #
 #
-#| warning: false
-library(ggplot2)
-library(palmerpenguins)
-
-ggplot(
-    data = penguins,
-    aes(
-        x = bill_length_mm,
-        y = bill_depth_mm
-    )
-) +
-  geom_point(aes(color = species)) +
-  hrbrthemes::theme_ipsum()
 #
 #
 #
 #
-#| warning: false
-library(ggplot2)
-library(palmerpenguins)
-ggplot(
-    data = penguins,
-    aes(
-        x = bill_length_mm,
-        y = bill_depth_mm
-    )
-) +
-  geom_point(aes(color = species)) +
-  hrbrthemes::theme_modern_rc()
+#
+#
 #
 #
 #
@@ -118,22 +60,39 @@ ggplot(
 #
 #
 #| warning: false
-library(ggplot2)
-library(palmerpenguins)
-ggplot(
-    data = penguins,
-    aes(
-        x = bill_length_mm,
-        y = bill_depth_mm
+#| echo: false
+data_nobel %>%
+    ggplot(aes(x = Chocolate, y = Nobel)) +
+    ggflags::geom_flag(aes(country = Flag)) +
+    ggpubr::stat_cor(
+        method = "pearson",
+        label.x = .5,
+        label.y = 27,
+        p.accuracy = 0.001,
+        r.accuracy = 0.01,
+        size = 6
+    ) +
+    ggrepel::geom_text_repel(
+        aes(label = Country),
+        box.padding = 0.5,
+        point.padding = 0.5
+    ) +
+    labs(
+        title = "More Chocolate Consumption, More Nobel Prizes",
+        subtitle = "Why Correlation is Not Causation",
+        x = "Chocolate consumption\n(kg/year/capita)",
+        y = "Nobel laureates\n(per 10 million people)",
+        caption = "Data: Messerli (2012) , New England Journal of Medicine"
+    ) +
+    hrbrthemes::theme_ipsum(
+        axis_text_size = 16,
+        axis_title_size = 18
+    ) +
+    theme(
+        plot.title = element_text(size = 25),
+        plot.caption = element_text(size = 14),
+        plot.subtitle = element_text(size = 20)
     )
-) +
-  ggforce::geom_mark_ellipse( # <1>
-    aes(fill = species), # <2>
-    alpha = 0.05, # <3>
-    color = 'transparent' # <4>
-  ) +
-  geom_point(aes(color = species)) +
-  jtools::theme_nice()
 ```
 #
 #
@@ -146,637 +105,79 @@ ggplot(
 #
 #
 #
+#
+data_nyt <- read.csv("http://clsong.com/assets/class_data/data_nyt.csv")
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
 #| warning: false
-library(ggplot2)
-library(palmerpenguins)
+#| echo: false
 
-ggplot(
-    data = penguins,
-    aes(
-        x = bill_length_mm,
-        y = bill_depth_mm
-    )
-) +
-  ggdensity::geom_hdr(aes(fill = species)) + # <1>
-  # geom_point(shape = 21) +  # <2>
-  jtools::theme_nice()
-```
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-library(ggplot2)
-library(palmerpenguins)
-ggplot(
-    data = penguins,
-    aes(
-        x = bill_length_mm,
-        y = bill_depth_mm
-    )
-) +
-  ggforce::geom_mark_ellipse(
-    aes(fill = species),
-    alpha = 0.05, 
-    color = 'transparent' 
-  ) +
+library(tidyverse)
+library(ggiraph)
+
+data_nyt %>% 
+  ggplot(aes(GDP, Doses)) +
   geom_point(
-    aes(fill = species), # <1>
-    color = "white", # <2>
-    shape = 21, # <3>
-    alpha = .6, # <4>
-    size = 3 # <5>
-  ) +
-  jtools::theme_nice()
-```
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-library(ggplot2)
-library(palmerpenguins)
-ggplot(
-    data = penguins,
-    aes(
-        x = bill_length_mm,
-        y = bill_depth_mm
-    )
-) +
-  geom_point(
-    aes(
-      fill = species, 
-      size = body_mass_g
-    ), 
+    aes(size = Population,
+        fill = Continent),
+    alpha = 0.5,
     shape = 21,
-    color = "transparent",
-    alpha = .3
+    color = 'transparent'
   ) +
-  geom_point(
-    aes(
-      size = body_mass_g
-    ), 
-    shape = 21,
-    color = "white",
-    fill = "transparent"
+  labs(
+    x = "G.D.P. per capita",
+    y = "doses administered per 100 people",
+    title = "Wealthier countries have administered more COVID-19 vacacines",
+    subtitle = "Circles are sized by country population"
   ) +
-  jtools::theme_nice()
-```
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-library(ggplot2)
-library(palmerpenguins)
-ggplot(
-    data = penguins,
-    aes(
-        x = bill_length_mm,
-        y = bill_depth_mm
-    )
-) +
-  ggforce::geom_mark_ellipse(
-    aes(fill = species),
-    alpha = 0.05,
-    color = 'transparent' 
+  scale_x_log10(
+    breaks = c(1000, 10000, 100000),
+    labels = scales::label_currency(),
+    limits = c(1000, 120000)
   ) +
-  geom_point(
-    aes(
-      shape = species,
-      fill = species # <1>
-    ), 
-    color = "white",
-    size = 3,
-    alpha = .6
+  scale_y_continuous(
+    limits = c(0, 250)
   ) +
-  scale_shape_manual(values = c(21, 22, 23)) + # <2>
-  jtools::theme_nice()
-```
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-library(ggplot2)
-library(palmerpenguins)
-p <- ggplot(
-    data = penguins,
-    aes(
-        x = bill_length_mm,
-        y = bill_depth_mm
-    )
-) +
-  ggforce::geom_mark_ellipse(
-    aes(fill = species),
-    alpha = 0.05,
-    color = 'transparent' 
-  ) +
-  geom_point(
-    aes(
-      shape = species,
-      fill = species 
-    ), 
-    color = "white",
-    size = 3,
-    alpha = .6
-  ) +
-  scale_shape_manual(values = c(21, 22, 23)) + 
-  scale_fill_manual( # <1>
-    values = c(
-      "Adelie" = "#00AFBB",  # <2>
-      "Chinstrap" = "#E7B800",  # <3>
-      "Gentoo" = "#FC4E07" # <4>
-    ) 
-  ) +
-  jtools::theme_nice()
-
-p
-```
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-library(ggplot2)
-library(palmerpenguins)
-ggplot(
-    data = penguins,
-    aes(
-        x = bill_length_mm,
-        y = bill_depth_mm
-    )
-) +
-  geom_point(
-    aes(fill = species),
-    color = "white", 
-    shape = 21, 
-    alpha = .6, 
-    size = 3 
+  scale_size(
+    guide = 'none',
+    range = c(1, 12)
   ) +
   scale_fill_manual(
-    values = c("#00AFBB", "#E7B800", "#FC4E07") # <1>
+    name = "",
+    values = MoMAColors::moma.colors("Klein", n=6, type="discrete")
   ) +
-  jtools::theme_nice()
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-p + 
-  labs( 
-    x = "Bill Length (mm)", # <1>
-    y = "Bill Depth (mm)" # <2>
-  )
-```
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-#| source-line-numbers: "6"
-p + 
-  labs(
-    x = "Bill Length (mm)",
-    y = "Bill Depth (mm)",
-    title = "Bill Length and Depth of Penguins are Positively Correlated" # <1>
-  )
-```
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-#| source-line-numbers: "8"
-p1 <- p + 
-  labs(
-    x = "Bill Length (mm)",
-    y = "Bill Depth (mm)",
-    title = "Bill Length and Depth are Positively Correlated"
-  ) +
-  geom_smooth(
-    aes(group = species, color = species), # <1>
-    method = "lm", se = FALSE  # <2>
-  ) +
-  scale_color_manual( # <3>
-    values = c(
-      "Adelie" = "#00AFBB",  
-      "Chinstrap" = "#E7B800", 
-      "Gentoo" = "#FC4E07" 
-    ) 
-  )
-
-p1
-```
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-p2 <- p1 +
-  theme( # <1>
-    axis.text = element_text(size = 12), # <2>
-    axis.title = element_text(size = 14),  # <3>
-    plot.title = element_text(size = 16), # <4>
-    legend.text = element_text(size = 12) # <5>
-  )
-p2
-```
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-#| source-line-numbers: "4"
-p2 +
-  theme(
-    legend.position = c(0.12, 0.1) # <1>
-  )
-```
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-#| source-line-numbers: "4"
-p2 +
-  theme(
-    legend.position = "top"  # <1>
-    # legend.position = "bottom" # <2>
-  )
-```
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-p3 <- p2 +
-  theme(
-    legend.position = c(0.12, 0.1),
-    legend.title = element_blank() # <1>
-  )
-
-p3
-```
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-p2 +
-  theme(
-    legend.position = "top"
-  ) +
-  labs(
-    color = "Penguin species",  # <1>
-    shape = "Penguin species",  # <2>
-    fill = "Penguin species",  # <3>,
-    linetype = "Penguin species"  # <4>
-  )
-```
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-
-p2 +
-    theme(legend.position = "none") +
-    annotate("text", # <1>
-        x = 33, y = 14, # <2>
-        label = "Adelie", # <3>
-        color = "#00AFBB", size = 5 # <4>
-    ) + 
-    annotate("text",
-        x = 55, y = 22,
-        label = "Chinstrap", color = "#E7B800", size = 5
-    ) +
-    annotate("text",
-        x = 58, y = 14,
-        label = "Gentoo", color = "#FC4E07", size = 5
-    )
-```
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-library(ggtext)
-p2 +
-  theme(
-    legend.position = "none"
-  ) +
-  labs(
-    subtitle = "Penguin Species: 
-    <span style = 'color:#00AFBB;'>**Adelie**</span><span style = 'color:#00AFBB;font-size:22pt'>\u25CF</span>,
-    <span style = 'color:#E7B800;'>**Chinstrap**</span><span style = 'color:#E7B800;font-size:20pt'>\u25A0</span>,
-    <span style = 'color:#FC4E07;'>**Gentoo**</span><span style = 'color:#FC4E07;font-size:22pt'>\u2666</span>"
+  hrbrthemes::theme_ft_rc(
+    axis_text_size = 12,
+    axis_title_size = 12,
+    plot_title_size = 14
   ) +
   theme(
-    plot.title = element_text(hjust = 0.5),
-    plot.subtitle = element_textbox_simple(halign = 0, size = 12)
+    legend.text=element_text(size=12),
+    legend.position = c(.15, .8),
+    legend.key.size = unit(0.15, 'in'),
+    panel.grid.minor = element_blank()
   )
-```
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-#| eval: false
-ggsave(
-  filename = "plot.pdf", # <1>
-  plot = p3, # <2>
-  width = 8, # <3>
-  height = 6, # <4>
-  units = "in", # <5>
-  device = cairo_pdf
-)
-```
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-#| eval: false
-ggsave(
-  filename = "plot.png", # <1>
-  plot = p3, 
-  width = 8, 
-  height = 6, 
-  units = "in", 
-  device = ragg::agg_png # <2>
-)
-```
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-
-library(ggplot2)
-library(palmerpenguins)
-library(ggiraph) # <1>
-
-p_interactive <- ggplot(
-    data = penguins,
-    aes(
-        x = bill_length_mm,
-        y = bill_depth_mm
-    )
-) +
-  geom_point_interactive(
-    aes(
-      color = species,
-      data_id = sex,
-      tooltip = body_mass_g
-    )
-  ) +
-  jtools::theme_nice() +
-  theme(legend.position = "top")
-
-girafe(ggobj = p_interactive)
-#
-#
-#
-#
-#
-#
-pacman::p_load(datasauRus)
-
-# datasaurus_dozen |>
-#   ggplot(aes(x = x, y = y, colour = dataset)) +
-#   geom_point() +
-#   theme_minimal() +
-#   facet_wrap(vars(dataset)) +
-#   labs(color = "Dataset")
-```
-#
 #
 #
 #
