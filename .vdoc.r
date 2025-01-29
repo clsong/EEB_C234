2f76db5a022541b4d5af5472ed90ec940d463490
Jane Doe <jane@example.org>
2025-01-29 09:00:34 -0800

    add quote usage

--- .vdoc.r
+++ .vdoc.r
@@ -3,49 +3,64 @@
 #
 #
 #
-#| message: false
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
 #| warning: false
 library(tidyverse)
 library(tidylog)
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#| message: false
 
-read_csv("data/per-capita-co-emissions.csv")
+# if you haven't installed dcldata, you can do so by running:
+# pak::pkg_install("dcl-docs/dcldata")
+library(dcldata)
 #
 #
 #
 #
 #
-#| message: false
-# if you don't have the janitor package installed, you can install it with the following command
-# pak::pkg_install("janitor")
+#
+#
+#
+#
+#
+#
+#
+example_eagle_nests
+#
+#
+#
+#
+#
+#| warning: false
 
-read_csv("data/per-capita-co-emissions.csv")  |> 
-    janitor::clean_names()
+eagle_nests_longer <- example_eagle_nests |>
+    pivot_longer( #<1>
+        cols = c(`2007`, `2009`), #<2>
+        names_to = "year", #<3>
+        values_to = "nests" #<4>
+    )
+eagle_nests_longer
+```
 #
 #
 #
@@ -53,10 +68,62 @@ read_csv("data/per-capita-co-emissions.csv")  |>
 #
 #
 #
-data_co2 <- read_csv("data/per-capita-co-emissions.csv")  |> 
-    janitor::clean_names()  |>  # <1>
-    rename(  # <2>
-        co2 = annual_co2_emissions_per_capita # <3>
+#
+#
+#
+#| warning: false
+#| eval: false
+
+example_eagle_nests |>
+    pivot_longer(
+        cols = -c(region), 
+        names_to = "year", 
+        values_to = "nests" 
+    )
+#
+#
+#
+#
+#
+#
+#
+#
+#
+library(tidyverse)
+
+billboard |>
+   pivot_longer(
+      cols = -c(______),
+      names_to = _____,
+      values_to = "rank"
+   ) 
+#
+#
+#
+#
+library(tidyverse)
+
+billboard |>
+   pivot_longer(
+      cols = -c(artist, track, date.entered),
+      names_to = "week",
+      values_to = "rank"
+   ) 
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| warning: false
+
+eagle_nests_longer |>
+    pivot_wider( #<1>
+        names_from = "year", #<2>
+        values_from = "nests" #<3>
     )
 ```
 #
@@ -66,14 +133,156 @@ data_co2 <- read_csv("data/per-capita-co-emissions.csv")  |>
 #
 #
 #
-data_co2 |> 
-    ggplot(aes(x = year, y = co2)) +
-    geom_line() + # <1>
-    labs(
-        x = "Year",
-        y = "Annual CO2 emissions per capita"
-    ) +
-    jtools::theme_nice()
+#
+#
+#
+#
+#
+#| warning: false
+
+library(palmerpenguins)
+
+penguins |>
+   group_by(species, sex) |>
+   summarise(mean_bill_length = mean(bill_length_mm)) 
+#
+#
+#
+#
+#
+#
+#| warning: false
+
+penguins |>
+   group_by(species, sex) |>
+   summarise(mean_bill_length = mean(bill_length_mm)) |>
+   ungroup() |>
+   pivot_wider(
+      names_from = "sex",
+      values_from = "mean_bill_length"
+   )
+#
+#
+#
+#
+#
+#
+#
+#| warning: false
+
+penguins |>
+   group_by(species, sex) |>
+   summarise(mean_bill_length = mean(bill_length_mm)) |>
+   ungroup() |>
+   pivot_wider(
+      names_from = "sex",
+      values_from = "mean_bill_length"
+   ) |>
+   ggplot(aes(male, female)) +
+   geom_point(size = 4) +
+   ggrepel::geom_text_repel(aes(label = species), box.padding = 1) +
+   geom_abline(intercept = 0, slope = 1) +
+   jtools::theme_nice()
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+library(tidyverse)
+
+us_rent_income |>
+   select(-moe) |>
+   pivot_wider(
+      names_from = ____,
+      values_from = ____
+   ) |>
+   mutate(rent_income_ratio = rent * 12 / income) |>
+   arrange(______)
+#
+#
+#
+#
+library(tidyverse)
+
+us_rent_income |>
+   select(-moe) |>
+   pivot_wider(
+      names_from = "variable",
+      values_from = "estimate"
+   ) |>
+   mutate(rent_income_ratio = rent * 12 / income) |>
+   arrange(rent_income_ratio)
+```   
+#
+#
+#
+#
+#
+#
+#
+example_gymnastics_2
+#
+#
+#
+#
+#
+#| warning: false
+
+example_gymnastics_2 |>
+    pivot_longer(
+        cols = -c(`country`), 
+        names_to = "event_year", 
+        values_to = "score"
+    )
+#
+#
+#
+#
+#
+#| warning: false
+
+example_gymnastics_2 |>
+    pivot_longer(
+        cols = -c(`country`), 
+        names_to = "event_year", 
+        values_to = "score"
+    ) |>
+    separate(
+        col = "event_year", 
+        into = c("event", "year"), 
+        sep = "_"
+    )
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+toy_dates <- tibble(
+  month = c(1, 2, 3),
+  day   = c(15, 20, 25),
+  year  = c(2020, 2020, 2020)
+)
+#
+#
+#
+#
+#
+toy_dates |>
+  unite( # <1>
+    col = "full_date", # <2>
+    month, day, year, # <3>
+    sep = "-" # <4>
+  )
 ```
 #
 #
@@ -82,53 +291,6 @@ data_co2 |>
 #
 #
 #
-#
-#
-#| message: false
-
-data_temp <- read_csv("data/temperature-anomaly.csv") |> 
-    janitor::clean_names()  |> 
-    select(year, global_average_temperature_anomaly_relative_to_1961_1990) |>
-    rename(temp_anomaly = global_average_temperature_anomaly_relative_to_1961_1990)
-
-data_temp
-#
-#
-#
-#
-#
-#| message: false
-
-data_co2  |> 
-    left_join(
-        data_temp, 
-        by = "year"
-    ) 
-#
-#
-#
-#
-#
-#
-#
-data_co2  |> 
-    left_join(
-        data_temp, 
-        by = "year"
-    )  |> 
-    drop_na() |>
-    ggplot(
-        aes(x = co2, 
-            y = temp_anomaly)
-        ) +
-    geom_smooth(se = FALSE) +
-    geom_point(aes(color = year)) +
-    scale_color_viridis_c() +
-    labs(
-        x = "Annual CO2 emissions per capita",
-        y = "Average temperature anomaly"
-    ) +
-    jtools::theme_nice()
 #
 #
 #
@@ -142,10 +304,16 @@ data_co2  |>
 #
 library(tidyverse)
 
-_____  |> 
-   left_join(
-        ____, 
-        by = _____
+who2 |>
+   pivot_longer(
+      cols = -c(______), 
+      names_to = "diagnosis_gender_age", 
+      values_to = "count"
+   ) |>
+   separate(
+      col = "diagnosis_gender_age",
+      into = c(______),
+      sep = ______
    )
 #
 #
@@ -154,19 +322,143 @@ _____  |>
 #
 library(tidyverse)
 
-band_members  |> 
-   left_join(
-        band_instruments, 
-        by = "name"
+who2 |>
+   pivot_longer(
+      cols = -c(country, year), 
+      names_to = "diagnosis_gender_age", 
+      values_to = "count"
+   ) |>
+   separate(
+      col = "diagnosis_gender_age",
+      into = c("diagnosis", "gender", "age"),
+      sep = "_"
    )
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+penguins
+#
+#
+#
+#
+#
+penguins |>
+  drop_na() # <1>
+```
+#
+#
+#
+#
+#
+penguins |>
+  drop_na(bill_length_mm) # <1>
+```
+#
+#
+#
+#
+#
+#
+#
+#
+#
+penguins |>
+  replace_na(
+   list(sex = 'male')
+  )
+#
+#
+#
+#
+#
+#
+#
+#
+library(tidyverse)
+# pak::pkg_install("datasauRus")
+library(datasauRus)
 
-band_instruments  |>
-    left_join(
-          band_members, 
-          by = "name"
-    )
+datasaurus_dozen_wide # <1>
+```
 #
 #
+#
+#
+#
+#
+datasaurus_dozen_long <- datasaurus_dozen_wide |>
+  janitor::clean_names() |>  # <1> 
+  mutate(row = row_number()) |>  # <2> 
+  pivot_longer(
+    cols = -"row",  # <3> 
+    names_to = "variable",  # <4> 
+    values_to = "value"  # <5> 
+  )
+```
+#
+#
+#
+#
+#
+#
+#
+#
+#
+datasaurus <- datasaurus_dozen_long |>
+  separate(
+    variable, 
+    into = c("dataset", "coord"),
+    sep = "_(?=[^_]*$)" # <1> # 
+  ) |>
+  pivot_wider( # <2>
+    names_from = "coord", 
+    values_from = "value"
+  )
+
+datasaurus
+```
+#
+#
+#
+#
+#
+#
+# Calculate summary statistics for each dataset
+datasaurus |>
+  group_by(dataset) |>
+  summarise(
+    mean_x = mean(x),
+    mean_y = mean(y),
+    sd_x = sd(x),
+    sd_y = sd(y),
+    cor = cor(x, y)
+  )
+#
+#
+#
+#
+#
+# Create faceted scatter plots
+datasaurus |>
+  ggplot(aes(x, y, color = dataset)) +
+  geom_point() +
+  facet_wrap(vars(dataset)) +
+  jtools::theme_nice() +
+  theme(legend.position = "none") +
+  labs(title = "The Datasaurus Dozen",
+       subtitle = "Same summary statistics, very different distributions")
 #
 #
 #
