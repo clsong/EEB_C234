1004b883bb1c64e78a187c7e7ed7407a6d9dabe7
clsong <chuliangsong1994@gmail.com>
2025-02-03 15:47:10 -0800

    add new chaoters

--- .vdoc.r
+++ .vdoc.r
@@ -11,252 +11,40 @@
 #
 #
 #
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
+#| message: false
 library(tidyverse)
 library(tidylog)
-
-# if you haven't installed dcldata, you can do so by running:
-# pak::pkg_install("dcl-docs/dcldata")
-library(dcldata)
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-example_eagle_nests
-#
-#
-#
-#
-#
-#| warning: false
-
-eagle_nests_longer <- example_eagle_nests |>
-    pivot_longer( #<1>
-        cols = c(`2007`, `2009`), #<2>
-        names_to = "year", #<3>
-        values_to = "nests" #<4>
-    )
-eagle_nests_longer
-```
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-#| eval: false
-
-example_eagle_nests |>
-    pivot_longer(
-        cols = -c(region), 
-        names_to = "year", 
-        values_to = "nests" 
-    )
-#
-#
-#
-#
-#
-#
-#
-#
-#
-library(tidyverse)
-
-billboard |>
-   pivot_longer(
-      cols = -c(______),
-      names_to = _____,
-      values_to = "rank"
-   ) 
-#
-#
-#
-#
-library(tidyverse)
-
-billboard |>
-   pivot_longer(
-      cols = -c(artist, track, date.entered),
-      names_to = "week",
-      values_to = "rank"
-   ) 
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-
-eagle_nests_longer |>
-    pivot_wider( #<1>
-        names_from = "year", #<2>
-        values_from = "nests" #<3>
-    )
-```
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-
 library(palmerpenguins)
-
+theme_set(theme_minimal()) #<1>
+```
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| warning: false
 penguins |>
-   group_by(species, sex) |>
-   summarise(mean_bill_length = mean(bill_length_mm)) 
+    ggplot(aes(x = flipper_length_mm)) +
+    geom_histogram(color = "white")
 #
 #
 #
 #
 #
-#
+#| echo: false
 #| warning: false
+library(patchwork)
 
-penguins |>
-   group_by(species, sex) |>
-   summarise(mean_bill_length = mean(bill_length_mm)) |>
-   ungroup() |>
-   pivot_wider(
-      names_from = "sex",
-      values_from = "mean_bill_length"
-   )
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-
-penguins |>
-   group_by(species, sex) |>
-   summarise(mean_bill_length = mean(bill_length_mm)) |>
-   ungroup() |>
-   pivot_wider(
-      names_from = "sex",
-      values_from = "mean_bill_length"
-   ) |>
-   ggplot(aes(male, female)) +
-   geom_point(size = 4) +
-   ggrepel::geom_text_repel(aes(label = species), box.padding = 1) +
-   geom_abline(intercept = 0, slope = 1) +
-   jtools::theme_nice()
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-library(tidyverse)
-
-us_rent_income |>
-   select(-moe) |>
-   pivot_wider(
-      names_from = ____,
-      values_from = ____
-   ) |>
-   mutate(rent_income_ratio = rent * 12 / income) |>
-   arrange(______)
-#
-#
-#
-#
-library(tidyverse)
-
-us_rent_income |>
-   select(-moe) |>
-   pivot_wider(
-      names_from = "variable",
-      values_from = "estimate"
-   ) |>
-   mutate(rent_income_ratio = rent * 12 / income) |>
-   arrange(rent_income_ratio)
-```   
-#
-#
-#
-#
-#
-#
-#
-example_gymnastics_2
-#
-#
-#
-#
-#
-#| warning: false
-
-example_gymnastics_2 |>
-    pivot_longer(
-        cols = -c(`country`), 
-        names_to = "eventandyear", 
-        values_to = "score"
-    )
-#
-#
-#
-#
-#
-#| warning: false
-
-example_gymnastics_2 |>
-    pivot_longer(
-        cols = -c(`country`), 
-        names_to = "eventandyear", 
-        values_to = "score"
+c(1, 5, 20) %>%
+    map(~ penguins |>
+        ggplot(aes(x = flipper_length_mm)) +
+        geom_histogram(binwidth = ., color = "white")
     ) |>
-    separate(
-        col = "eventandyear", 
-        into = c("event", "year"), 
-        sep = "_"
-    )
+    wrap_plots()
 #
 #
 #
@@ -265,119 +53,37 @@ example_gymnastics_2 |>
 #
 #
 #
-#
-#
-toy_dates <- tibble(
-  month = c(1, 2, 3),
-  day   = c(15, 20, 25),
-  year  = c(2020, 2020, 2020)
-)
-#
-#
-#
-#
-#
-toy_dates |>
-  unite( # <1>
-    col = "full_date", # <2>
-    month, day, year, # <3>
-    sep = "-" # <4>
-  )
-```
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-library(tidyverse)
-
-who2 |>
-   pivot_longer(
-      cols = -c(______), 
-      names_to = "diagnosis_gender_age", 
-      values_to = "count"
-   ) |>
-   separate(
-      col = "diagnosis_gender_age",
-      into = c(______),
-      sep = ______
-   )
-#
-#
-#
-#
-#
-library(tidyverse)
-
-who2 |>
-   pivot_longer(
-      cols = -c(country, year), 
-      names_to = "diagnosis_gender_age", 
-      values_to = "count"
-   ) |>
-   separate(
-      col = "diagnosis_gender_age",
-      into = c("diagnosis", "gender", "age"),
-      sep = "_"
-   )
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-penguins
-#
-#
-#
-#
-#
+#| warning: false
 penguins |>
-  drop_na() # <1>
-```
+    ggplot(aes(x = flipper_length_mm)) +
+    geom_density(fill = "lightblue")
 #
 #
 #
 #
 #
+#| warning: false
 penguins |>
-  drop_na(bill_length_mm) # <1>
+    drop_na(flipper_length_mm) |>
+    mutate(flipper_length_mm_normalized = (flipper_length_mm - min(flipper_length_mm)) / (max(flipper_length_mm) - min(flipper_length_mm))) |>
+    ggplot(aes(x = flipper_length_mm_normalized)) +
+    geom_density(fill = "lightblue") +
+    xlim(-.2, 1.2) # <1>
 ```
 #
 #
 #
 #
 #
-#
-#
-#
-#
+#| warning: false
+#| source-line-numbers: "6"
 penguins |>
-  replace_na(
-   list(sex = 'male') # <1>
-  )
+    drop_na(flipper_length_mm) |>
+    mutate(flipper_length_mm_normalized = (flipper_length_mm - min(flipper_length_mm)) / (max(flipper_length_mm) - min(flipper_length_mm))) |>
+    ggplot(aes(x = flipper_length_mm_normalized)) +
+    geom_density(fill = "lightblue", 
+                bounds = c(0, 1)) + # <1>
+    xlim(-.2, 1.2)
 ```
 #
 #
@@ -387,27 +93,74 @@ penguins |>
 #
 #
 #
-library(tidyverse)
-# pak::pkg_install("datasauRus")
-library(datasauRus)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| warning: false
+penguins |> 
+    ggplot(aes(x = flipper_length_mm, group = species, fill = species)) +
+    geom_histogram(color = "white", alpha = .5,position = "identity") +
+    theme(legend.position = "top")
+#
+#
+#
+#
+#
+#
+#
+#| warning: false
+penguins |>
+    ggplot(aes(x = flipper_length_mm, group = species, fill = species)) +
+    geom_histogram(color = "white", position = "stack") +
+    theme(legend.position = "top")
+#
+#
+#
+#
+#
+#
+#
+#| warning: false
+penguins |> 
+    ggplot(aes(x = flipper_length_mm, group = species, fill = species)) +
+    geom_histogram(color = "white", position = "dodge") +
+    theme(legend.position = "top")
+#
+#
+#
+#
+#
+#
+#
+#| warning: false
+penguins |> 
+    ggplot(aes(
+        x = flipper_length_mm, 
+        group = species, 
+        fill = species, 
+        color = species)) +
+    geom_density(alpha = .5) +
+    theme(legend.position = "top")
+#
+#
+#
+#
+#
+#| warning: false
+library(geomtextpath)
 
-datasaurus_dozen_wide # <1>
-```
-#
-#
-#
-#
-#
-#
-datasaurus_dozen_long <- datasaurus_dozen_wide |>
-  janitor::clean_names() |>  # <1> 
-  mutate(row = row_number()) |>  # <2> 
-  pivot_longer(
-    cols = -"row",  # <3> 
-    names_to = "variable",  # <4> 
-    values_to = "value"  # <5> 
-  )
-```
+penguins |> 
+    ggplot(aes(x = flipper_length_mm, group = species, color = species)) +
+    geom_textdensity(alpha = .5, aes(label = species), size = 5) +
+    theme(legend.position = "none")
 #
 #
 #
@@ -417,49 +170,187 @@ datasaurus_dozen_long <- datasaurus_dozen_wide |>
 #
 #
 #
-datasaurus <- datasaurus_dozen_long |>
-  separate(
-    variable, 
-    into = c("dataset", "coord"),
-    sep = "_(?=[^_]*$)" # <1> 
-  ) |>
-  pivot_wider( # <2>
-    names_from = "coord", 
-    values_from = "value"
-  )
+#
+#
+#
+#
+#| warning: false
+library(gghighlight)
 
-datasaurus
+penguins |> 
+    ggplot(aes(x = flipper_length_mm, fill = species)) +
+    geom_histogram(color = "white") +
+    gghighlight() +
+    facet_wrap(~species)
+#
+#
+#
+#
+#
+#
+#
+#| warning: false
+penguins |> 
+    ggplot(aes(x = flipper_length_mm)) +
+    geom_density(
+        data = filter(penguins, sex == "male"),
+        aes(y = ..density..),
+        alpha = .5, fill = "#69b3a2", color = "black") +
+    geom_label( aes(x=180, y=0.02, label="male"), color="#69b3a2", size = 5) +
+    geom_density(
+        data = filter(penguins, sex == "female"),
+        aes(y = -..density..),
+        alpha = .5, fill = "#404080", color = "black") +
+    geom_label( aes(x=228, y=-0.01, label="female"), color="#404080", size = 5)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+library(ggridges)
+lincoln_weather_clean <- lincoln_weather |>
+    select(CST, `Mean Temperature [F]`) |>
+    mutate(CST = ymd(CST)) |>
+    mutate(month = month(CST, label = TRUE))
+
+lincoln_weather_clean
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+lincoln_weather_clean |>
+    group_by(month) |>
+    summarise(mean = mean(`Mean Temperature [F]`),
+              lower = quantile(`Mean Temperature [F]`, 0.025),
+              upper = quantile(`Mean Temperature [F]`, 0.975)) |>
+    ggplot(aes(x = month, y = mean, ymin = lower, ymax = upper)) +
+    geom_pointrange()
+#
+#
+#
+#
+#
+lincoln_weather_clean |>
+    group_by(month) |>
+    summarise(mean = mean(`Mean Temperature [F]`),
+              sd = sd(`Mean Temperature [F]`)) |>
+    ggplot(aes(x = month, y = mean)) +
+    geom_pointrange(aes(ymin = mean - sd, ymax = mean + sd))
+#
+#
+#
+#
+#
+lincoln_weather_clean |>
+    group_by(month) |>
+    summarise(mean = mean(`Mean Temperature [F]`),
+              se = sd(`Mean Temperature [F]`) / sqrt(n())) |>
+    ggplot(aes(x = month, y = mean)) +
+    geom_pointrange(aes(ymin = mean - se, ymax = mean + se))
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| warning: false
+lincoln_weather_clean |> 
+    ggplot(aes(x = month, y = `Mean Temperature [F]`)) +
+    geom_boxplot() 
+#
+#
+#
+#
+#
+#| warning: false
+lincoln_weather_clean |> 
+    ggplot(aes(x = month, y = `Mean Temperature [F]`)) +
+    geom_violin() 
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+lincoln_weather_clean |> 
+    ggplot(aes(x = month, y = `Mean Temperature [F]`)) +
+    geom_point(alpha = .5)
+#
+#
+#
+#
+#
+lincoln_weather_clean |> 
+    ggplot(aes(x = month, y = `Mean Temperature [F]`)) +
+    geom_jitter(alpha = .5)
+#
+#
+#
+#
+#
+#
+lincoln_weather_clean |> 
+    ggplot(aes(x = month, y = `Mean Temperature [F]`)) +
+    ggbeeswarm::geom_quasirandom(varwidth = TRUE) 
 ```
 #
 #
 #
 #
 #
-#
-# Calculate summary statistics for each dataset
-datasaurus |>
-  group_by(dataset) |>
-  summarise(
-    mean_x = mean(x),
-    mean_y = mean(y),
-    sd_x = sd(x),
-    sd_y = sd(y),
-    cor = cor(x, y)
-  )
+lincoln_weather_clean |> 
+    ggplot(aes(x = month, y = `Mean Temperature [F]`)) +
+    geom_violin(alpha = .5) +
+    ggforce::geom_sina()
 #
 #
 #
 #
 #
-# Create faceted scatter plots
-datasaurus |>
-  ggplot(aes(x, y, color = dataset)) +
-  geom_point() +
-  facet_wrap(vars(dataset)) +
-  jtools::theme_nice() +
-  theme(legend.position = "none") +
-  labs(title = "Same summary statistics, very different distributions")
 #
+#
+#| warning: false
+library(ggridges)
+lincoln_weather_clean |> 
+    ggplot(aes(
+        x = `Mean Temperature [F]`, 
+        y = month, 
+        fill = month, 
+        color = month
+    )) +
+    geom_density_ridges(alpha = .5) +
+    theme_ridges() +
+    theme(legend.position = "none") 
 #
 #
 #
