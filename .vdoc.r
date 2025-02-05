fb9689f7dabb2591113a532dcd1772c83efa22c8
Jane Doe <jane@example.org>
2025-02-04 23:53:00 -0800

    upload new materials

--- .vdoc.r
+++ .vdoc.r
@@ -4,6 +4,48 @@
 #
 #
 #
+#| warning: false
+#| message: false
+library(tidyverse)
+library(tidylog)
+library(palmerpenguins)
+theme_set(cowplot::theme_cowplot())
+#
+#
+#
+#
+#
+#
+#
+#
+#
+penguins |>
+    ggplot(aes(x = bill_length_mm, y = bill_depth_mm)) +
+    geom_point() +
+    geom_rug()
+#
+#
+#
+#
+#
+#
+#
+#
+library(ggside)
+
+penguins |>
+    ggplot(aes(x = bill_length_mm, y = bill_depth_mm, color = species)) +
+    geom_point() +
+    geom_xsidedensity(aes(y = after_stat(density))) + # <1>
+    geom_ysidedensity(aes(x = after_stat(density))) + # <2>
+    theme(
+        ggside.axis.text = element_blank() # <3>
+    )
+```
+#
+#
+#
+#
 #
 #
 #
@@ -12,39 +54,31 @@
 #
 #
 #| message: false
-library(tidyverse)
-library(tidylog)
-library(palmerpenguins)
-theme_set(theme_minimal()) #<1>
-```
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-penguins |>
-    ggplot(aes(x = flipper_length_mm)) +
-    geom_histogram(color = "white")
-#
-#
-#
-#
-#
-#| echo: false
-#| warning: false
-library(patchwork)
+data(lending_club, package = "modeldata")
 
-c(1, 5, 20) %>%
-    map(~ penguins |>
-        ggplot(aes(x = flipper_length_mm)) +
-        geom_histogram(binwidth = ., color = "white")
-    ) |>
-    wrap_plots()
+lending_club_log10 <- lending_club |>
+    mutate(annual_inc = log10(annual_inc)) |>
+    mutate(total_il_high_credit_limit = log10(total_il_high_credit_limit)) |>
+    filter(is.finite(annual_inc), is.finite(total_il_high_credit_limit))
+
+lending_club_log10 |>
+    ggplot(aes(x = annual_inc, y = total_il_high_credit_limit)) +
+    geom_point()
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| warning: false
+lending_club_log10 |>
+    ggplot(aes(x = annual_inc, y = total_il_high_credit_limit)) +
+    geom_point(alpha = 0.05) # <1>
+```
+#
 #
 #
 #
@@ -54,21 +88,9 @@ c(1, 5, 20) %>%
 #
 #
 #| warning: false
-penguins |>
-    ggplot(aes(x = flipper_length_mm)) +
-    geom_density(fill = "lightblue")
-#
-#
-#
-#
-#
-#| warning: false
-penguins |>
-    drop_na(flipper_length_mm) |>
-    mutate(flipper_length_mm_normalized = (flipper_length_mm - min(flipper_length_mm)) / (max(flipper_length_mm) - min(flipper_length_mm))) |>
-    ggplot(aes(x = flipper_length_mm_normalized)) +
-    geom_density(fill = "lightblue") +
-    xlim(-.2, 1.2) # <1>
+lending_club_log10 |>
+    ggplot(aes(x = annual_inc, y = total_il_high_credit_limit)) +
+    ggdensity::geom_hdr() # <1>
 ```
 #
 #
@@ -76,14 +98,10 @@ penguins |>
 #
 #
 #| warning: false
-#| source-line-numbers: "6"
-penguins |>
-    drop_na(flipper_length_mm) |>
-    mutate(flipper_length_mm_normalized = (flipper_length_mm - min(flipper_length_mm)) / (max(flipper_length_mm) - min(flipper_length_mm))) |>
-    ggplot(aes(x = flipper_length_mm_normalized)) +
-    geom_density(fill = "lightblue", 
-                bounds = c(0, 1)) + # <1>
-    xlim(-.2, 1.2)
+lending_club_log10 |>
+    ggplot(aes(x = annual_inc, y = total_il_high_credit_limit)) +
+    ggdensity::geom_hdr_lines() + # <1>
+    geom_point(pch='.', color = 'black', size = 0.5, alpha = 0.05) # <2>
 ```
 #
 #
@@ -101,256 +119,84 @@ penguins |>
 #
 #
 #
-#
-#
-#
-#| warning: false
-penguins |> 
-    ggplot(aes(x = flipper_length_mm, group = species, fill = species)) +
-    geom_histogram(color = "white", alpha = .5,position = "identity") +
-    theme(legend.position = "top")
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-penguins |>
-    ggplot(aes(x = flipper_length_mm, group = species, fill = species)) +
-    geom_histogram(color = "white", position = "stack") +
-    theme(legend.position = "top")
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-penguins |> 
-    ggplot(aes(x = flipper_length_mm, group = species, fill = species)) +
-    geom_histogram(color = "white", position = "dodge") +
-    theme(legend.position = "top")
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-penguins |> 
-    ggplot(aes(
-        x = flipper_length_mm, 
-        group = species, 
-        fill = species, 
-        color = species)) +
-    geom_density(alpha = .5) +
-    theme(legend.position = "top")
-#
-#
-#
-#
-#
-#| warning: false
-library(geomtextpath)
-
-penguins |> 
-    ggplot(aes(x = flipper_length_mm, group = species, color = species)) +
-    geom_textdensity(alpha = .5, aes(label = species), size = 5) +
-    theme(legend.position = "none")
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-library(gghighlight)
-
-penguins |> 
-    ggplot(aes(x = flipper_length_mm, fill = species)) +
-    geom_histogram(color = "white") +
-    gghighlight() +
-    facet_wrap(~species)
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-penguins |> 
-    ggplot(aes(x = flipper_length_mm)) +
-    geom_density(
-        data = filter(penguins, sex == "male"),
-        aes(y = ..density..),
-        alpha = .5, fill = "#69b3a2", color = "black") +
-    geom_label( aes(x=180, y=0.02, label="male"), color="#69b3a2", size = 5) +
-    geom_density(
-        data = filter(penguins, sex == "female"),
-        aes(y = -..density..),
-        alpha = .5, fill = "#404080", color = "black") +
-    geom_label( aes(x=228, y=-0.01, label="female"), color="#404080", size = 5)
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-library(ggridges)
-lincoln_weather_clean <- lincoln_weather |>
-    select(CST, `Mean Temperature [F]`) |>
-    mutate(CST = ymd(CST)) |>
-    mutate(month = month(CST, label = TRUE))
-
-lincoln_weather_clean
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-lincoln_weather_clean |>
-    group_by(month) |>
-    summarise(mean = mean(`Mean Temperature [F]`),
-              lower = quantile(`Mean Temperature [F]`, 0.025),
-              upper = quantile(`Mean Temperature [F]`, 0.975)) |>
-    ggplot(aes(x = month, y = mean, ymin = lower, ymax = upper)) +
-    geom_pointrange()
-#
-#
-#
-#
-#
-lincoln_weather_clean |>
-    group_by(month) |>
-    summarise(mean = mean(`Mean Temperature [F]`),
-              sd = sd(`Mean Temperature [F]`)) |>
-    ggplot(aes(x = month, y = mean)) +
-    geom_pointrange(aes(ymin = mean - sd, ymax = mean + sd))
-#
-#
-#
-#
-#
-lincoln_weather_clean |>
-    group_by(month) |>
-    summarise(mean = mean(`Mean Temperature [F]`),
-              se = sd(`Mean Temperature [F]`) / sqrt(n())) |>
-    ggplot(aes(x = month, y = mean)) +
-    geom_pointrange(aes(ymin = mean - se, ymax = mean + se))
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-lincoln_weather_clean |> 
-    ggplot(aes(x = month, y = `Mean Temperature [F]`)) +
-    geom_boxplot() 
-#
-#
-#
-#
-#
-#| warning: false
-lincoln_weather_clean |> 
-    ggplot(aes(x = month, y = `Mean Temperature [F]`)) +
-    geom_violin() 
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-lincoln_weather_clean |> 
-    ggplot(aes(x = month, y = `Mean Temperature [F]`)) +
-    geom_point(alpha = .5)
-#
-#
-#
-#
-#
-lincoln_weather_clean |> 
-    ggplot(aes(x = month, y = `Mean Temperature [F]`)) +
-    geom_jitter(alpha = .5)
-#
-#
-#
-#
-#
-#
-lincoln_weather_clean |> 
-    ggplot(aes(x = month, y = `Mean Temperature [F]`)) +
-    ggbeeswarm::geom_quasirandom(varwidth = TRUE) 
+lending_club_log10 |>
+    ggplot(aes(x = annual_inc, y = total_il_high_credit_limit)) +
+    ggpointdensity::geom_pointdensity() +
+    scale_color_viridis_c() # <1>
 ```
 #
 #
 #
 #
 #
-lincoln_weather_clean |> 
-    ggplot(aes(x = month, y = `Mean Temperature [F]`)) +
-    geom_violin(alpha = .5) +
-    ggforce::geom_sina()
-#
-#
-#
 #
 #
 #
 #
 #| warning: false
-library(ggridges)
-lincoln_weather_clean |> 
-    ggplot(aes(
-        x = `Mean Temperature [F]`, 
-        y = month, 
-        fill = month, 
-        color = month
-    )) +
-    geom_density_ridges(alpha = .5) +
-    theme_ridges() +
-    theme(legend.position = "none") 
+data(ex0826, package = "Sleuth3")
+
+ex0826 |>
+    ggplot(aes(x = Mass, y = Metab)) +
+    geom_point() +
+    scale_x_log10( 
+        breaks = scales::trans_breaks("log10", function(x) 10^x),
+        labels = scales::trans_format("log10", scales::math_format(10^.x))
+    ) +
+    scale_y_log10(
+        breaks = scales::trans_breaks("log10", function(x) 10^x),
+        labels = scales::trans_format("log10", scales::math_format(10^.x))
+    ) +
+    annotation_logticks() +
+    geom_smooth(method = "lm") +
+    ggpmisc::stat_quant_eq()
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+penguins_small <- penguins |>
+    select(species, bill_length_mm, bill_depth_mm, flipper_length_mm, body_mass_g) |>
+    drop_na()
+#
+#
+#
+#
+#
+#
+#
+#| message: false
+#| warning: false
+penguins_small |>
+    GGally::ggpairs()
+#
+#
+#
+#
+#
+#
+#
+library(ggpca)
+penguins_small |>
+    ggpca(
+        metadata_cols = "species",  # <1>
+        mode = "pca",               # <2>
+        color_var = "species",      # <3>
+        ellipse = TRUE              # <4>
+    )
+```
+#
+#
+#
+#
+#
+#
 #
 #
 #
