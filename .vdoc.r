d06c6f53f5cc432e0e3481aa24778608830dd95c
clsong <chuliangsong1994@gmail.com>
2025-02-05 16:03:30 -0800

    add pset2

--- .vdoc.r
+++ .vdoc.r
@@ -4,197 +4,205 @@
 #
 #
 #
-#| warning: false
-#| message: false
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| eval: false
 library(tidyverse)
-library(tidylog)
-library(palmerpenguins)
-theme_set(cowplot::theme_cowplot())
-#
-#
-#
-#
-#
-#
-#
-#
-#
-penguins |>
-    ggplot(aes(x = bill_length_mm, y = bill_depth_mm)) +
-    geom_point() +
-    geom_rug()
-#
-#
-#
-#
-#
-#
-#
-#
-library(ggside)
+library(readxl) # <1>
+library(lubridate) # <2>
 
-penguins |>
-    ggplot(aes(x = bill_length_mm, y = bill_depth_mm, color = species)) +
-    geom_point() +
-    geom_xsidedensity(aes(y = after_stat(density))) + # <1>
-    geom_ysidedensity(aes(x = after_stat(density))) + # <2>
+data_ts <- read_excel("_____", col_types = _______) |>
+    janitor::clean_names() |>
+    mutate(date = ymd(date) - years(1900)) # <3>
+
+
+data_temperature <- read_excel("______",
+    sheet = "______", col_types = c(_______)
+) |>
+    janitor::clean_names()
+```
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| echo: false
+#| warning: false
+library(tidyverse)
+library(readxl)
+library(lubridate)
+
+data_ts <- read_excel("data/pnas.1421968112.sd01.xlsx",
+    col_types = c(
+        "date", "numeric", "skip",
+        "skip", "numeric", "skip", "skip",
+        "skip", "skip", "skip", "numeric",
+        "skip", "skip"
+    )
+) |>
+    janitor::clean_names() |>
+    mutate(date = ymd(date) - years(1900))
+
+
+data_temperature <- read_excel("data/pnas.1421968112.sd01.xlsx",
+    sheet = "Temperature Data Fig.2A", col_types = c(
+        "date", "numeric", "skip", "skip", "skip", "skip"
+    )
+) |>
+    janitor::clean_names()
+
+data <- data_ts |>
+    left_join(data_temperature)
+
+data |>
+    pivot_longer(cols = -c(date, temperature_o_c), names_to = "species", values_to = "abundance") |>
+    mutate(
+        species = str_replace(species, "_percent", "")
+    )
+#
+#
+#
+#
+#
+#
+#
+#| eval: false
+#| echo: false
+library(tidyverse)
+library(readxl)
+library(lubridate)
+
+data_ts <- read_excel("data/pnas.1421968112.sd01.xlsx",
+    col_types = c(
+        "date", "numeric", "skip",
+        "skip", "numeric", "skip", "skip",
+        "skip", "skip", "skip", "numeric",
+        "skip", "skip"
+    )
+) |>
+    janitor::clean_names() |>
+    mutate(date = ymd(date) - years(1900))
+
+
+data_temperature <- read_excel("data/pnas.1421968112.sd01.xlsx",
+    sheet = "Temperature Data Fig.2A", col_types = c(
+        "date", "numeric", "skip", "skip", "skip", "skip"
+    )
+) |>
+    janitor::clean_names()
+
+data <- data_ts |>
+    left_join(data_temperature)
+
+data_long <- data |>
+    pivot_longer(cols = -c(date, temperature_o_c), names_to = "species", values_to = "abundance")
+
+data_long  |> 
+    group_by(species) |> 
+    summarise(
+        cor = cor(temperature_o_c, abundance, use = "complete.obs")
+    )
+
+library(gghighlight)
+ggthemr::ggthemr(palette = "fresh")
+
+data_long %>%
+    ggplot(aes(date, abundance)) +
+    geom_line(aes(group = species, color = species)) +
+    gghighlight(use_direct_label = F) +
+    facet_wrap(~species) +
+    jtools::theme_nice() +
+    labs(
+        x = "Time",
+        y = "Abundance"
+    ) +
     theme(
-        ggside.axis.text = element_blank() # <3>
+        legend.position = "none"
     )
-```
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#| message: false
-data(lending_club, package = "modeldata")
 
-lending_club_log10 <- lending_club |>
-    mutate(annual_inc = log10(annual_inc)) |>
-    mutate(total_il_high_credit_limit = log10(total_il_high_credit_limit)) |>
-    filter(is.finite(annual_inc), is.finite(total_il_high_credit_limit))
-
-lending_club_log10 |>
-    ggplot(aes(x = annual_inc, y = total_il_high_credit_limit)) +
-    geom_point()
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-lending_club_log10 |>
-    ggplot(aes(x = annual_inc, y = total_il_high_credit_limit)) +
-    geom_point(alpha = 0.05) # <1>
-```
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-lending_club_log10 |>
-    ggplot(aes(x = annual_inc, y = total_il_high_credit_limit)) +
-    ggdensity::geom_hdr() # <1>
-```
-#
-#
-#
-#
-#
-#| warning: false
-lending_club_log10 |>
-    ggplot(aes(x = annual_inc, y = total_il_high_credit_limit)) +
-    ggdensity::geom_hdr_lines() + # <1>
-    geom_point(pch='.', color = 'black', size = 0.5, alpha = 0.05) # <2>
-```
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-lending_club_log10 |>
-    ggplot(aes(x = annual_inc, y = total_il_high_credit_limit)) +
-    ggpointdensity::geom_pointdensity() +
-    scale_color_viridis_c() # <1>
-```
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#| warning: false
-data(ex0826, package = "Sleuth3")
-
-ex0826 |>
-    ggplot(aes(x = Mass, y = Metab)) +
-    geom_point() +
-    scale_x_log10( 
-        breaks = scales::trans_breaks("log10", function(x) 10^x),
-        labels = scales::trans_format("log10", scales::math_format(10^.x))
+data_long %>%
+    ggplot(aes(temperature_o_c, abundance)) +
+    geom_point(aes(color = species)) +
+    facet_wrap(~species) +
+    labs(
+        x = "Temperature (°C)",
+        y = "Abundance"
     ) +
-    scale_y_log10(
-        breaks = scales::trans_breaks("log10", function(x) 10^x),
-        labels = scales::trans_format("log10", scales::math_format(10^.x))
-    ) +
-    annotation_logticks() +
-    geom_smooth(method = "lm") +
-    ggpmisc::stat_quant_eq()
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-penguins_small <- penguins |>
-    select(species, bill_length_mm, bill_depth_mm, flipper_length_mm, body_mass_g) |>
-    drop_na()
-#
-#
-#
-#
-#
-#
-#
-#| message: false
-#| warning: false
-penguins_small |>
-    GGally::ggpairs()
-#
-#
-#
-#
-#
-#
-#
-library(ggpca)
-penguins_small |>
-    ggpca(
-        metadata_cols = "species",  # <1>
-        mode = "pca",               # <2>
-        color_var = "species",      # <3>
-        ellipse = TRUE              # <4>
+    jtools::theme_nice() +
+    theme(
+        legend.position = "none"
     )
-```
-#
-#
-#
-#
 #
 #
 #
